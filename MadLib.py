d2484d5f60c82f768e73b7bec06e0ae3bc65b8fe
agnidhis <nidhiagarwal@unomaha.edu>
2026-01-20 17:43:09 +0000

    Completed MadLibm lab1

--- MadLib.py
+++ MadLib.py
@@ -6,10 +6,15 @@
 def main():
   print("Madlib")
   #Ask user for words
+  word = input ("Please enter a word:")
+  word1=input( "please enter another word:")
+  word2=input("Another word please:")
 
   #Print the story with the user supplied words.
-
-
+  print("You used the word " + word + " for this assignment i created.")
+  print("I am suppose to use the second word " + word1 + " somewhere in the story but i dont know where.")
+  print("Finally the last word is " + word2 + " which is i finally added in this story" + " :)")
+  
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
